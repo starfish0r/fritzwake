df390e6d13aaf308b6f5fd57d8ec995c365057c0
Christian Abraham <abraham@traveltainment.de>
2017-09-20 16:55:40 +0200

    struc2

--- src/main/java/de/cabraham/fritzwake/Wake.java
+++ src/main/java/de/cabraham/fritzwake/Wake.java
@@ -4,6 +4,8 @@ public class Wake {
 
   public static void main(String[] args) {
     
+    //xhr=1&sid=4d726b0484f36fc9&lang=de&no_sidrenew=&dev_name=0starfish2&dev_ip=192.168.1.25&static_dhcp=on&allow_pcp_and_upnp=on&kisi_profile=filtprof1&back_to_page=%2Fnet%2Fnet_overview.lua&dev=landevice1101&last_action=&btn_wake=&oldpage=%2Fnet%2Fedit_device.lua
+    
 
   }
 
